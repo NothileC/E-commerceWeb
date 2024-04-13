ebf3e549160163343e482274da4e8e679bbc30df
Zuribaby <chiamakanwobodo07@gmail.com>
2024-04-12 23:53:56 -0800

    creating the add payment section

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -1,4 +1,12 @@
 import React from 'react';
 import '../css/Payment.css';
 
-
+function Payment() {
+  return (
+    <div className='first-card' id='first'>
+      <div className='type'>
+        <h1>SELECT A CARD</h1>
+      </div>
+    </div>
+  )
+}
