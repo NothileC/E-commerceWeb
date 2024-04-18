84fa6cf0e282bcc3b5aa7f8402d4cf0c5d9d5345
ThandokuhleNdlebe <thandotydx@gmail.com>
2024-04-18 15:15:50 +0200

    sidebar images successfully added

--- src/sidebar.js
+++ src/sidebar.js
@@ -7,17 +7,20 @@ const Sidebar = () => {
             {/* Add your sidebar content here */}
             <ul>
                 <li>
-                <img src="" alt="Home" />
+                <img src="./img/Logo.png" alt="Logo"/>
                 </li>
                 <li>
-                <img src="path_to_about_image" alt="About" />
+                <img src="./img/lines.png" alt="More"/>
                 </li>
                 <li>
-                <img src="path_to_contact_image" alt="Contact" />
+                <img src="./img/sidebaricon2.png" alt="Store"/>
+                </li>
+                <li>
+                <img src="./img/shoppingbag.png" alt="Bag"/>
                 </li>
             </ul>
         </div>
-    );
+    );    
 };
 
 export default Sidebar;
