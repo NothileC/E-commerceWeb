1cda1100471f6fdb468a913d158f6f1fc048c51e
Aeden Manell <nthabelengaedenmanell@gmail.com>
2024-04-20 01:30:30 +0200

    comments

--- src/pages/Bag.js
+++ src/pages/Bag.js
@@ -8,25 +8,32 @@ import Sidenav from '../components/sidenav/Sidenav';
 const Bag = () => {
   return (
     <div>
-
-      <div className='columns'>      
-        <div className='column1' >
-          <Sidenav />
+      {/* The Bag component represents a page where users can view the items in their bag */}
+      <div className='columns'>
+        {/* The layout is divided into three columns */}
+        <div className='column1'>
+          {/* The left column contains a side navigation component */}
+          <Sidenav /> {/* Renders the side navigation component */}
         </div>
-        <div className='column-2' >
-        <h1 className='bagTitle'>Check your Bag Items</h1>
+        <div className='column-2'>
+          {/* The middle column contains the bag items */}
+          <h1 className='bagTitle'>Check your Bag Items</h1>
           <div className='bagsection'>
-            <Cart />
+            <Cart /> {/* Renders the component that displays the bag items */}
           </div>
         </div>
-        <div className='column-3' >
+        <div className='column-3'>
+          {/* The right column contains the bag summary and checkout button */}
           <div>
             <h1 id='heading-cart'>Bag</h1>
           </div>
           <div className='sidecart'>
-            <Cart />
+            <Cart /> {/* Renders the component that displays the bag summary */}
           </div>
-          <Link to="/checkout" className="ViewBag-Btn"> <img src={bagIcon} className='iconBag' /></Link>
+          <Link to="/checkout" className="ViewBag-Btn">
+            {/* Renders a link to the checkout page */}
+            <img src={bagIcon} className='iconBag' /> {/* Renders an icon for the bag/checkout */}
+          </Link>
         </div>
       </div>
     </div>
--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -5,19 +5,21 @@ import { ShippingAddress } from '../components/cartsummary/shippingaddress';
 import { PaymentMethod } from '../components/cartsummary/paymentmethod';
 import { Cart } from '../components/cart/cart';
 
+// The Checkout component represents a page where users can review and complete their purchase.
 const Checkout = () => {
   return (
     <div className='columns-cartsummary'>
-      
+      {/* The layout is divided into two columns */}
       <div className='column1checkout'>
-        <ShippingAddress />
-        <PaymentMethod />
-        <Cart />
+        {/* The left column contains the user's shipping address, payment method, and cart */}
+        <ShippingAddress /> {/* Renders the component for entering the shipping address */}
+        <PaymentMethod /> {/* Renders the component for selecting the payment method */}
+        <Cart /> {/* Renders the component that displays the items in the cart */}
       </div>
       <div className='column2checkout'>
-          <CartSummary />
+          {/* The right column contains the summary of the cart */}
+          <CartSummary /> {/* Renders the component that displays the summary of the cart */}
       </div>
-
     </div>
   );
 };
