3802f4a270df279f7c286bd8020f11a2ff04eb7a
Aeden Manell <nthabelengaedenmanell@gmail.com>
2024-04-20 01:16:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Cart } from "./components/cart/cart";
+import { AppleStoreContextProvider } from "./components/context/AppleStoreContext";
+import Dashboard from "./pages/Dashboard";
+import Bag from "./pages/Bag";
+import Checkout from "./pages/Checkout";
 
-
-
-const App = () => {
-
-
-
-
+function App() {
   return (
-    <div>
-    
+    <div className="App">
+      <AppleStoreContextProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/bag" element={<Bag />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout /> } />
+          </Routes>
+        </Router>
+      </AppleStoreContextProvider>
     </div>
   );
-};
+}
 
 export default App;
